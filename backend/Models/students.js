8be2f3db12d95975ad1b70d9e2d5e1534fd80eb9
0xInterruptHandler <173845338+0xInterruptHandler@users.noreply.github.com>
2025-08-06 16:08:48 -0500

    modelo 1

---
+++ backend/Models/students.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const SchemaUsuario = new mongoose.Schema({
+    nombre: { type: String, required: true },
+    correo: { type: String, required: true, unique: true },
+    password: { type: String, required: true },  
+    rol: { type: String, enum: ['Admin', 'Profesor', 'Estudiante'], required: true },  
+    idReferencia: { type: mongoose.Schema.Types.ObjectId },  
+    FechaCreacion: { type: Date, default: Date.now }
+});
+
+export const ModeloUsuario = mongoose.model('Usuario', SchemaUsuario);
+
+
+export default ModeloUsuario
